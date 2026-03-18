b6f2ef25d3935f5fb4fb210435c3e67d3fb3ef45
AHang05 <148489862+AHang05@users.noreply.github.com>
2026-03-18 12:37:52 +0700

    feat: Add Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,6 @@
+import LandingPage from "@/app/pages/home/page";
 
 export default function Home() {
   return (
-    <section className="bg-background p-20">
-      <span className="text-amber-deep tracking-widest text-xs uppercase mb-4">
-        Project Overview
-      </span>
-      <h1 className="text-text-main text-7xl">
-        The <span className="italic-serif text-amber">368 MW</span> <br />
-        Smart Grid Initiative
-      </h1>
-      <p className="text-text-sub mt-6 max-w-xl">
-        Sustainable urban development in the future under the development framework 
-        of the Renewable Energy Power Plant Industry.
-      </p>
-      
-      <div className="mt-12 p-12 bg-bg-alt border border-border rounded-2xl">
-        <span className="text-amber text-2xl">1,557.3 Acres</span>
-        <p className="text-sm">Pilot project in Phichit City</p>
-      </div>
-    </section>
+    <LandingPage/>
   );}
---
+++ app/pages/home/page.tsx
@@ -0,0 +1,8 @@
+import Hero from './Hero'
+
+export default function LandingPage() {
+    return (
+        <Hero />
+    )
+
+}
